d47734a1d7ded15d3d377c8449b58978a0ff06d6
Boris Rose <boris.alexandre.rose@gmail.com>
2024-06-11 23:48:11 +0200

    feat/form

--- J2/formulaire/src/ts/form.js
+++ J2/formulaire/src/ts/form.js
@@ -3,7 +3,17 @@ function submitHandler(e) {
     e.preventDefault();
     const email = document.getElementById('email');
     const password = document.getElementById('password');
-    if (!email.value || !password.value) {
+    console.log('error', new RegExp('/^[a-zA-Z0-9\.\?\!éèùï]{8,12}$/g').test(password.value));
+    if (email.value.length < 10 || !(new RegExp('/^[a-zA-Z0-9\.\?\!éèùï]{8,12}$/', 'i').test(password.value))) {
+        console.log('error', new RegExp('/^[a-zA-Z0-9\.\?\!éèùï]{8,12}$/g').test(password.value));
+        const passwordError = document.getElementById('password-error');
+        const emailError = document.getElementById('email-error');
+        passwordError.innerHTML = `<small class="warning" style="opacity:0; color:red; font-weight:600; animation: show-up-warning 1000ms ease-in-out forwards"> Mot de passe incorrect ou manquant </small>`;
+        emailError.innerHTML = `<small class="warning" style="font-weight:600; color:red; animation:show-up-warning 1000ms ease-in-out forwards; opacity:0"> Email incorrect ou manquant </small>`;
+        setTimeout(() => {
+            passwordError.innerHTML = ``;
+            emailError.innerHTML = ``;
+        }, 5000);
         return;
     }
     else {
--- J2/formulaire/src/ts/form.ts
+++ J2/formulaire/src/ts/form.ts
@@ -3,10 +3,23 @@ const form:HTMLElement = document.getElementById("login-form");
 
 function submitHandler(e:any){
     e.preventDefault()
+
     const email = document.getElementById('email') as HTMLInputElement;
     const password = document.getElementById('password') as HTMLInputElement;
+    
+    console.log('error', new RegExp('/^[a-zA-Z0-9\.\?\!éèùï]{8,12}$/g').test(password.value))
 
-    if(!email.value || !password.value) {
+
+    if(email.value.length < 10 || !(new RegExp('/^[a-zA-Z0-9\.\?\!éèùï]{8,12}$/', 'i').test(password.value))) {
+        console.log('error', new RegExp('/^[a-zA-Z0-9\.\?\!éèùï]{8,12}$/g').test(password.value))
+        const passwordError = document.getElementById('password-error') as HTMLElement
+        const emailError = document.getElementById('email-error') as HTMLElement
+        passwordError.innerHTML = `<small class="warning" style="opacity:0; color:red; font-weight:600; animation: show-up-warning 1000ms ease-in-out forwards"> Mot de passe incorrect ou manquant </small>`
+        emailError.innerHTML = `<small class="warning" style="font-weight:600; color:red; animation:show-up-warning 1000ms ease-in-out forwards; opacity:0"> Email incorrect ou manquant </small>`
+        setTimeout(() => { 
+            passwordError.innerHTML = ``
+            emailError.innerHTML = ``
+        }, 5000)
         return 
     } else {
         
